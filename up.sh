597d107569d5b6b96d22236b0f5288b4a3b6e359
tony-waters <2tonywaters@gmail.com>
2026-04-04 19:37:55 +0100

    add convenience scripts

--- up.sh
+++ up.sh
@@ -3,6 +3,7 @@ cd terraform
 source kind create cluster
 terraform apply -var-file=terraform.tfvars.example -auto-approve
 cd ..
+
 kubectl apply -f helm/spring-boot-app-seed/seed-job.yaml
-kubectl logs -f spring-boot-app-seed-7gfvw -n spring-boot-app-demo
+#kubectl logs spring-boot-app-seed -n spring-boot-app-demo
 #watch kubectl get po -A
