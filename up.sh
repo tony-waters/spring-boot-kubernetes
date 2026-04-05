b978c00f847b9122bb40bcbb1dcc58c6b6f343d2
tony-waters <2tonywaters@gmail.com>
2026-04-05 19:46:53 +0100

    add tests after up

--- up.sh
+++ up.sh
@@ -7,3 +7,8 @@ cd ..
 kubectl apply -f helm/spring-boot-app-seed/seed-job.yaml
 #kubectl logs spring-boot-app-seed -n spring-boot-app-demo
 #watch kubectl get po -A
+
+# smoke test
+kubectl port-forward -n spring-boot-app-demo svc/spring-boot-app-spring-boot-app 8080:80
+curl -v http://localhost:8080/actuator/health
+curl -v http://localhost:8080/actuator/health/readiness
