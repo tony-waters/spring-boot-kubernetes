67463e6c8103ec401231bb1655c97ae82bf26bcd
Tony Waters <2tonywaters@gmail.com>
2026-07-10 13:43:32 +0100

    remove comments

--- terraform/up.sh
+++ terraform/up.sh
@@ -5,5 +5,5 @@ sleep 30
 terraform -chdir=infra init
 terraform -chdir=infra apply -auto-approve
 
-#terraform -chdir=app init
-#terraform -chdir=app apply -auto-approve
+terraform -chdir=app init
+terraform -chdir=app apply -auto-approve
